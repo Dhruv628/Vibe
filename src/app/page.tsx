ae2a07a78dfb2efad7591c82d110ab87ac1a8ba0
unknown <dhruv@samaaro.com>
2025-11-11 17:07:41 +0530

    05 : AI Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export function Page() {
+  const [value, setValue] = useState("")
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess:()=>toast.success("Background job invoked successfully"),
   }))
   const invokeBackgroundJob = async () => {
-    invoke.mutate({text:"John doe"})
+    invoke.mutate({value: value})
   }
   return (
     <div className="px-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e)=>setValue(e.target.value)} />
       <Button disabled={invoke.isPending} onClick={invokeBackgroundJob}>
         Invoke Background Job
       </Button>
--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,14 +1,38 @@
+// lib/inngest/functions.ts
+import { OpenAI } from "openai";
 import { inngest } from "./client";
 
+const perplexity = new OpenAI({
+  apiKey: process.env.PERPLEXITY_API_KEY,
+  baseURL: "https://api.perplexity.ai",
+});
+
 export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
+  { id: "summarize-contents" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    // download video
-    await step.sleep("wait-a-moment", "10s");
     
-    // transcribe video
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    // Use step.run to wrap Perplexity calls for retry/observability
+    const output = await step.run("code-agent", async () => {
+      const response = await perplexity.chat.completions.create({
+        model: "sonar-pro",
+        messages: [
+          {
+            role: "system",
+            content: "You are an expert developer in next.js. You write readable and maintainable code. You write simple next.js and react.js snippets",
+          },
+          {
+            role: "user",
+            content: `Write the following snippet: ${event.data.value}`,
+          },
+        ],
+        temperature: 0.7,
+        max_tokens: 3000,
+      });
+
+      return response.choices[0].message?.content || "";
+    });
+
+    return { output };
+  }
 );
