431ee2d2617fa08ea411e3ad3b9c8b14f6f208d7
unknown <dhruv@samaaro.com>
2025-11-22 16:56:22 +0530

    FIX : bug

---
+++ src/modules/projects/ui/views/components/messages-container.tsx
@@ -0,0 +1,59 @@
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query"
+import { MessageCard } from "./message-card";
+import { MessageForm } from "./message-form";
+import { useEffect, useRef } from "react";
+import { MessageRole } from "@/generated/prisma/enums";
+
+type Props = {
+    projectId : string,
+}
+
+export const MessagesContainer = ({ projectId } : Props) => {
+    const bottomRef = useRef<HTMLDivElement>(null);
+    const trpc = useTRPC();
+    const { data: messages } = useSuspenseQuery(trpc.messages.getMany.queryOptions({
+        projectId
+    }))
+    useEffect(() => {
+      const lastAssistantMessage = messages.findLast(message => message.role === MessageRole.ASSISTANT)
+      if(lastAssistantMessage) {
+        // TODO : set active fragment
+      }
+    }, [messages])
+
+    useEffect(() => {
+      bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [messages.length])
+    
+    
+    return (
+        <div className="flex flex-col flex-1 min-h-0">
+            <div className="flex-1 min-h-0 overflow-y-auto">
+                <div className="pt-1 pr-1">
+                    {
+                        messages.map( ( message ) => {
+                            return (
+                                <MessageCard
+                                    key={message.id}
+                                    content={message.content}
+                                    role={message.role}
+                                    fragment={message.fragment}
+                                    createdAt={message.createdAt}
+                                    isActiveFragment={false}
+                                    onFragmentClick={ () => {} }
+                                    type={message.type}
+                                 />
+                            )
+                        })
+                    }
+                </div>
+                <div ref={bottomRef} />
+            </div>
+            <div className="relative p-3 pt-1">
+                <div className="absolute -top-6 left-0 right-0 h-6 bg-linear-to-b from-transparent-to-background/70 pointer-events-none" />
+                <MessageForm projectId={projectId} />
+            </div>
+        </div>
+    )
+}
