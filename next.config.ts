efb8618a9ba656ee6d5dd05db5cd09ada5afc518
unknown <dhruv@samaaro.com>
2025-12-03 16:55:32 +0530

    22 : deployment bug fixes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client');
-    }
-    return config;
-  },
-  outputFileTracingIncludes: {
-    '/api/**/*': ['./src/generated/prisma/**/*'],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
